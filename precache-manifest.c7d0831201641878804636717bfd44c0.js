35bcc3516e52b8083f72d7291c82a814f4dbc04d
Daniel Whiffing <danielwhiffing@gmail.com>
2020-08-09 13:05:56 -0400

    Updates

---
+++ precache-manifest.c7d0831201641878804636717bfd44c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7499f2a9d60c55562a02236eb1943813",
+    "url": "/cleanup98-48/index.html"
+  },
+  {
+    "revision": "039dc490e8efc79eb3a0",
+    "url": "/cleanup98-48/static/css/2.e5a4b2eb.chunk.css"
+  },
+  {
+    "revision": "35acb8ac2799ad8d388f",
+    "url": "/cleanup98-48/static/css/main.df1af8db.chunk.css"
+  },
+  {
+    "revision": "039dc490e8efc79eb3a0",
+    "url": "/cleanup98-48/static/js/2.5cc61475.chunk.js"
+  },
+  {
+    "revision": "4aaab955bf0d4d647c1c1084b1c89dfc",
+    "url": "/cleanup98-48/static/js/2.5cc61475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35acb8ac2799ad8d388f",
+    "url": "/cleanup98-48/static/js/main.236bab1e.chunk.js"
+  },
+  {
+    "revision": "e9a111e920e823a366bb",
+    "url": "/cleanup98-48/static/js/runtime-main.08e5c7dd.js"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/cleanup98-48/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "/cleanup98-48/static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "/cleanup98-48/static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/cleanup98-48/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
